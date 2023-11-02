3ea5b0f12b2d5ad09bc47d5a84cfc552452c70c0
YL Wang <121748352+code4yonglei@users.noreply.github.com>
2023-11-02 13:03:24 +0100

    updating exercises

---
+++ exercise/day_4_script_grep_find_sed_awk/c_hello.c
@@ -0,0 +1,13 @@
+//
+// cc hello.c 
+//
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("\n----------------------\n");
+    printf("Hello World from CPU compiled using GCC!\n");
+    printf("----------------------\n\n");
+    return 0;
+}
+
---
+++ exercise/day_4_script_grep_find_sed_awk/cuda_hello.cu
@@ -0,0 +1,24 @@
+//
+// nvcc -arch sm_60 -O3 -o xxx hello.cu
+//
+#include <stdio.h>
+
+void __global__ hello_from_gpu()
+{
+    printf("\n\n---Hello World from the GPU!\n\n");
+}
+
+
+int main(int argc, const char * argv[])
+{
+	printf("\n----------------------\n");
+	printf("Hello World from CPU! Before calling 'hello_from_gpu' kernel function.\n");
+	
+    hello_from_gpu<<<1, 1>>>();
+
+	printf("Hello World from CPU!  After calling 'hello_from_gpu' kernel function.\n");
+	printf("\n----------------------\n");
+
+	cudaDeviceSynchronize(); //cudaDeviceReset();
+    return 0;
+}
